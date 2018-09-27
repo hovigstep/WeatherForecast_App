354985f6d6e49f60ac7cc607e2353dce79887918
HovigStep <hovigstep@gmail.com>
2018-09-27 13:19:52 -0700

    BindViewUpdate

--- app/src/main/java/com/weatherforecast_app/weatherforecast_app/MainActivity.java
+++ app/src/main/java/com/weatherforecast_app/weatherforecast_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.weatherforecast_app.weatherforecast_app;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.SparseIntArray;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,9 +14,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+    private SparseIntArray mErrorString = new SparseIntArray();
+    private static final int REQUEST_PERMISSIONS = 20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +37,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        
+
     }
 
 
