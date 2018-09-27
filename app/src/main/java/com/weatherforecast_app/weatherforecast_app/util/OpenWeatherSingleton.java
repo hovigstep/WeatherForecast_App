9fba034eff9a11b17443c2bd91c34f8576e5fe66
HovigStep <hovigstep@gmail.com>
2018-09-27 12:44:15 -0700

    OpenweatherSGT

---
+++ app/src/main/java/com/weatherforecast_app/weatherforecast_app/util/OpenWeatherSingleton.java
@@ -0,0 +1,50 @@
+package com.weatherforecast_app.weatherforecast_app.util;
+
+import okhttp3.Call;
+import okhttp3.Request;
+
+public class OpenWeatherSingleton {
+    public OpenWeatherSingleton() {
+    }
+
+    public static OpenWeatherSingleton getInstance(){
+        if(instance == null){
+            instance = new OpenWeatherSingleton();
+        }
+        return instance;
+    }
+
+    public Call getCurrentWeatherByCity(String cityName){
+        return getCurrentWeatherByCity(cityName, "metric");
+    }
+
+    public Call getCurrentWeatherByCity(String cityName, String unit){
+        Request request = new Request.Builder()
+                .url(api_server + String.format("weather?q=%s&units=%s&appid=%s", cityName, unit, appid))
+                .build();
+
+        return client.newCall(request);
+    }
+
+    public Call getForecastByCity(String cityName){
+        return getForecastByCity(cityName, "metric", 5);
+    }
+
+    public Call getForecastByCity(String cityName, String unit, int cnt){
+        Request request = new Request.Builder()
+                .url(api_server + String.format("forecast/daily?q=%s&units=%s&cnt=%d&appid=%s", cityName, unit, cnt, appid))
+                .build();
+
+        return client.newCall(request);
+    }
+
+    public int getIconFromWeather(String weather){
+        weather = weather.toLowerCase();
+        if(weather.contains("cloud")) return R.drawable.zzz_weather_cloudy;
+        else if(weather.contains("snow")) return R.drawable.zzz_weather_snowy;
+        else if(weather.contains("rain") || weather.contains("drizzle")) return R.drawable.zzz_weather_rainy;
+        else if(weather.contains("fog")) return R.drawable.zzz_weather_fog;
+        else if(weather.contains("lightning")) return R.drawable.zzz_weather_lightning;
+        return R.drawable.zzz_weather_sunny;
+    }
+}
