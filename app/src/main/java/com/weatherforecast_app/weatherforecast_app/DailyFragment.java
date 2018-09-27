f858ee0c1d26b0033781189e6077f08c8171e085
HovigStep <hovigstep@gmail.com>
2018-09-27 12:21:18 -0700

    DailyFragment_Update

--- app/src/main/java/com/weatherforecast_app/weatherforecast_app/DailyFragment.java
+++ app/src/main/java/com/weatherforecast_app/weatherforecast_app/DailyFragment.java
@@ -1,4 +1,48 @@
 package com.weatherforecast_app.weatherforecast_app;
 
-public class DailyFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+public class DailyFragment extends Fragment {
+
+    public DailyFragment() {
+        // Empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mCity = getArguments().getString(ARG_CITY);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflating layout for fragment
+        View view = inflater.inflate(R.layout.fragment_daily, container, false);
+        ButterKnife.bind(this, view);
+
+        mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshData();
+            }
+        });
+        refreshData();
+
+        return view;
+    }
+
+    public void changeCity(String city){
+        mCity = city;
+        refreshData();
+    }
+
+
 }
