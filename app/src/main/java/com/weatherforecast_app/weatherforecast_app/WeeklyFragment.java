8fbeb47b453dc0bf6a8cf29070573950b096fc2b
HovigStep <hovigstep@gmail.com>
2018-09-27 12:24:51 -0700

    WeeklyFragmentClass_Update

--- app/src/main/java/com/weatherforecast_app/weatherforecast_app/WeeklyFragment.java
+++ app/src/main/java/com/weatherforecast_app/weatherforecast_app/WeeklyFragment.java
@@ -1,4 +1,49 @@
 package com.weatherforecast_app.weatherforecast_app;
 
-public class WeeklyFragment {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+public class WeeklyFragment extends Fragment {
+
+    public WeeklyFragment() {
+        // Required empty public constructor
+    }
+
+    public static WeeklyFragment newInstance(String location) {
+        WeeklyFragment fragment = new WeeklyFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_CITY, location);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mCity = getArguments().getString(ARG_CITY);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_weekly, container, false);
+        ButterKnife.bind(this, view);
+        SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) view;
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshData();
+            }
+        });
+        refreshData();
+        return view;
+    }
 }
