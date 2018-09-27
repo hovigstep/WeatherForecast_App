a728d934884b9974f61cbc8628c722690b872dcc
HovigStep <hovigstep@gmail.com>
2018-09-27 13:53:24 -0700

    SettingsFragment_Update

--- app/src/main/java/com/weatherforecast_app/weatherforecast_app/SettinggsFragment.java
+++ app/src/main/java/com/weatherforecast_app/weatherforecast_app/SettinggsFragment.java
@@ -1,4 +1,17 @@
 package com.weatherforecast_app.weatherforecast_app;
 
-public class SettinggsFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettinggsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+    }
+    @Override
+    public void onCreatePreferencesFix(@Nullable Bundle savedInstanceState, String rootKey) {
+
+    }
 }
