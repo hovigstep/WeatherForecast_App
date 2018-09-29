848f17314e24d6ce5c6cf1dfc17633428935d750
HovigStep <hovigstep@gmail.com>
2018-09-29 12:26:28 -0700

    Metadata&permissions_update

--- app/src/main/java/com/weatherforecast_app/weatherforecast_app/MapFrament.java
+++ app/src/main/java/com/weatherforecast_app/weatherforecast_app/MapFrament.java
@@ -1,7 +1,14 @@
 package com.weatherforecast_app.weatherforecast_app;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 
-public class MapFrament extends Fragment {
-
+public class MapFrament extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_fragment);
+    }
 }
