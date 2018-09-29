d9a812aa174c4a4bfbad57bf6868dad9afcd887d
HovigStep <hovigstep@gmail.com>
2018-09-29 12:11:18 -0700

    Is_serviceOK_update

--- app/src/main/java/com/weatherforecast_app/weatherforecast_app/MainActivity.java
+++ app/src/main/java/com/weatherforecast_app/weatherforecast_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.weatherforecast_app.weatherforecast_app;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -16,6 +17,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.preference.PreferenceManager;
+import android.util.Log;
 import android.util.SparseIntArray;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -26,6 +28,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -41,6 +47,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     NavigationView navigationView;
 
     private SparseIntArray mErrorString = new SparseIntArray();
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
     private static final int REQUEST_PERMISSIONS = 20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +67,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 }, R.string.runtime_permissions_txt
                 , REQUEST_PERMISSIONS);
         displayView(R.id.nav_daily_forecast);
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 
     @Override
