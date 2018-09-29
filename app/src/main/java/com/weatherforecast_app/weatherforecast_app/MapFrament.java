cece128cb9702fa1e16a637f62a531651e205837
HovigStep <hovigstep@gmail.com>
2018-09-29 13:18:53 -0700

    LocationPermissionCheck_update

--- app/src/main/java/com/weatherforecast_app/weatherforecast_app/MapFrament.java
+++ app/src/main/java/com/weatherforecast_app/weatherforecast_app/MapFrament.java
@@ -181,7 +181,28 @@ public class MapFrament extends FragmentActivity implements OnMapReadyCallback {
         mapFragment.getMapAsync(MapFrament.this);
     }
 
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {android.Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
 
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionsGranted = true;
+                initMap();
+            }else{
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -205,6 +226,10 @@ public class MapFrament extends FragmentActivity implements OnMapReadyCallback {
                 }
             }
         }
+    }
+
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
 }
